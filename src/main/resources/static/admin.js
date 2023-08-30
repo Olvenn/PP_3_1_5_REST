1db4b47ab43811640bda137aa6847afac6928361
Olvenn <olga-vencel@yandex.ru>
2023-08-30 23:15:17 +0300

    Fix admin.js.

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,16 +1,35 @@
 const userUrl = 'http://localhost:8090/api/users';
 
+const userContainer = document.querySelector(".content-users__main");
 const userNameContainer = document.querySelector(".navbar__user");
 const userRoleContainer = document.querySelector(".navbar__role");
 
 fetch(userUrl)
     .then(response => response.json())
     .then(user => {
-        console.log(user[0].body.name);
+        console.log(user[1].body.name);
         addDataInHeader(user[0].body);
+        createUserItem(user[1].body)
     });
 
 console.log("start")
+
+const createUserItem = (userNext) => {
+    const user = `  
+         <td class="bg-light" th:text="${userNext.id}">UserName</td>
+         <td class="bg-light" th:text="${userNext.name}">UserName</td>
+         <td class="bg-light" th:text="${userNext.surname}">UserName</td>
+         <td class="bg-light" th:text="${userNext.email}">Email</td>
+         <td class="d-flex">
+
+          </td>
+`
+    userContainer.innerHTML = user;
+};
+
+// <p className="bg-light">
+//     ${userNext.role.map(role => createRoleItem(role)).join(' ')}
+// </p>
 
 const createRoleItem = (role) => (
     `<span style="margin-right: 5px">${role.name.replace('ROLE_', '')}</span>`
